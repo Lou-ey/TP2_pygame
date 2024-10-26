a8e7c9668760cc0da147191503a26993fd985b3b
Rodri <nunes90rodrigo@gmail.com>
2024-10-26 01:16:45 +0100

    Criei um monstro teste

--- controller.py
+++ controller.py
@@ -5,8 +5,7 @@ pygame.init()
 
 def main():
     game_scene = GameScene() # Instancia a cena do jogo
-    run = True
-    while run:
+    while True:
         game_scene.run() # Roda o jogo
         pygame.display.update() # Atualiza a tela
 
